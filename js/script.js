b8489858a1b0678eababc9bc4c6451e2dbaa32d7
azaazazaa <56756165+azaazazaa@users.noreply.github.com>
2020-04-12 16:23:59 +0500

    Добавлена форма редактирования профиля

--- js/script.js
+++ js/script.js
@@ -155,3 +155,26 @@ const nextLvl9 = document.querySelector('.skill__anim_nom3-3');
                 anim9.classList.add('skill__anim_pic9-active');
             }
         })
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+  console.log(scrollbar);
+  document.querySelector('[href="#open-modal"]').addEventListener('click', function() {
+    document.body.style.overflow = 'hidden';
+    document.querySelector('#openModal').style.marginLeft = scrollbar;
+  });
+  document.querySelector('[href="#close"]').addEventListener('click',function(){
+    document.body.style.overflow = 'visible';
+    document.querySelector('#openModal').style.marginLeft = '0px';
+  });
+})
+
+
+
+let onTop = document.getElementById('on-top');
+        onTop.addEventListener('click', function() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0;
+        });
+
